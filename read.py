3bec28744b1280cf9bebab050a934fc72b422d46
felixwzh <felixwzh@outlook.com>
2016-12-05 18:50:16 +0800

    = =

--- read.py
+++ read.py
@@ -15,49 +15,90 @@ def get_items(source_file_name):
     return items
 #
 
-def get_user_list(source_file_name,item_list):
-    #get user from file and add it to the user_list_all,which includes all the users from all the files
+def get_user_list_04(source_file_name,item_list):
+    # get user from file and add it to the user_list_all,which includes all the users from all the files
     input = open(source_file_name,'r')
     flag = 0
     for line in input:
         if flag != 0:
             line=line.strip('\n')
-            user={}.fromkeys(item_all,'None')
             s1 = re.split('\t', line)
-            # #
-            user_flag = 0
-            for pre_user in user_list_all:
-                if 'CONS_NO' in item_list:
-                    if pre_user['CONS_NO'] == s1[item_list.index('CONS_NO')] and s1[item_list.index('CONS_NO')]!='None':
-                        CONS_NO=pre_user['CONS_NO']
-                        for i in range(0,len(s1)):
-                            if s1[i] != '':
-                                pre_user[item_list[i]] = s1[i]
-                        pre_user['CONS_NO']=CONS_NO
-                        user_flag =1
-                        break
-                if 'CONS_ID' in item_list:
-                    if pre_user['CONS_ID'] == s1[item_list.index('CONS_ID')] and s1[item_list.index('CONS_ID')]!='None' :
-                        CONS_ID=pre_user['CONS_ID']
-                        for i in range(0,len(s1)):
-                            if s1[i] != '':
-                                pre_user[item_list[i]] = s1[i]
-                        pre_user['CONS_ID']=CONS_ID
-                        user_flag = 1
-                        break
-            if user_flag==1 : continue
-
+            user={}.fromkeys(item_all,'None')
             for i in range(0,len(s1)):
                 if s1[i] != '':
                     user[item_list[i]] = s1[i]
-            # #
-            if user['CONS_ID']=='None':
-                user['CONS_ID']=user['CONS_NO']
-            if user['CONS_NO']=='None':
-                user['CONS_NO']=user['CONS_ID']
+
             user_list_all.append(user)
-        else: flag=1
+            CONS_ID_list[user['CONS_ID']]=1
+            CONS_NO_list[user['CONS_NO']]=1
+        flag=1
     input.close()
+
+
+def get_user_list(source_file_name,item_list):
+    #get user from file and add it to the user_list_all,which includes all the users from all the files
+    if 'CONS_NO' in item_list:
+        input = open(source_file_name,'r')
+        flag = 0
+        for line in input:
+            if flag != 0:
+                line=line.strip('\n')
+                user={}.fromkeys(item_all,'None')
+                s1 = re.split('\t', line)
+                # #
+                user_flag = 0
+                if s1[item_list.index('CONS_NO')] in CONS_NO_list:
+                    for pre_user in user_list_all:
+                        if pre_user['CONS_NO'] == s1[item_list.index('CONS_NO')] and s1[item_list.index('CONS_NO')]!='None':
+                            CONS_NO=pre_user['CONS_NO']
+                            for i in range(0,len(s1)):
+                                if s1[i] != '':
+                                    pre_user[item_list[i]] = s1[i]
+                            pre_user['CONS_NO']=CONS_NO
+                            user_flag =1
+                            break
+                    if user_flag==1 : continue
+
+                for i in range(0,len(s1)):
+                    if s1[i] != '':
+                        user[item_list[i]] = s1[i]
+                # #
+                user['CONS_ID']=user['CONS_NO']
+                CONS_NO_list[user['CONS_NO']]=1
+                user_list_all.append(user)
+            else: flag=1
+        input.close()
+    elif 'CONS_ID' in item_list:
+        input = open(source_file_name,'r')
+        flag = 0
+        for line in input:
+            if flag != 0:
+                line=line.strip('\n')
+                user={}.fromkeys(item_all,'None')
+                s1 = re.split('\t', line)
+                # #
+                user_flag = 0
+                if s1[item_list.index('CONS_ID')] in CONS_ID_list:
+                    for pre_user in user_list_all:
+                        if pre_user['CONS_ID'] == s1[item_list.index('CONS_ID')] and s1[item_list.index('CONS_ID')]!='None' :
+                            CONS_ID=pre_user['CONS_ID']
+                            for i in range(0,len(s1)):
+                                if s1[i] != '':
+                                    pre_user[item_list[i]] = s1[i]
+                            pre_user['CONS_ID']=CONS_ID
+                            user_flag = 1
+                            break
+                    if user_flag==1 : continue
+
+                for i in range(0,len(s1)):
+                    if s1[i] != '':
+                        user[item_list[i]] = s1[i]
+                # #
+                user['CONS_NO']=user['CONS_ID']
+                CONS_ID_list[user['CONS_ID']]=1
+                user_list_all.append(user)
+            else: flag=1
+        input.close()
 
 #
 # def get_user_list(source_file_name,item_list):
@@ -76,6 +117,7 @@ def get_user_list(source_file_name,item_list):
 #             user_list_all.append(user)
 #         flag=1
 #     input.close()
+
 
 def encode(item_list,user_list,*index):
     #encode to one hot code and out put the data in one hot in "data_onehot.txt"
@@ -114,13 +156,13 @@ def encode(item_list,user_list,*index):
         user_list_new.append(new_user)
 
 
-    print "start OneHotEncoder"
+
     #onehot encode
     enc = OneHotEncoder()
     enc.fit(user_list_new)
     user_list_onehot = enc.transform(user_list_new).toarray()
     # user in user_list_onehot,user[1]==1 means this user has a label; user[0]==1,means this user dosen't have a label
-    print "OneHotEncoder end"
+
 
 
     #build the dict of every categorical data
@@ -133,62 +175,54 @@ def encode(item_list,user_list,*index):
                 +enc.feature_indices_[i+1]
         item_onehot_index_dict_dict[encode_item[i]]=item_onehot_index_dict
 
-    print "start write item_onehot_index_dict.txt"
     #out put this dict to "item_onehot_index_dict.txt"
     output_index=open("item_onehot_index_dict.txt","w")
     for item in item_onehot_index_dict_dict:
         for value in item_onehot_index_dict_dict[item]:
             output_index.write(item+'\t'+value+'\t'+str(item_onehot_index_dict_dict[item][value])+'\n')
     output_index.close()
-    print "end write item_onehot_index_dict.txt"
 
 
 
-    # output the uservec ignore the None value
-    print "start write data_onehot.txt"
-    output_onehot=open("data_onehot.txt","w")
-    for user in user_list_onehot:
-        # output label
-        output_onehot.write(str(int(user[1]))+'\t')
-        # output index of one
-        for i in range(2,len(user)):
-            if user[i]!=0.0:
-                if i not in enc.feature_indices_:
-                    output_onehot.write(str(i)+'\t')
-        output_onehot.write('\n')
-    output_onehot.close()
-
-    print "end write data_onehot.txt"
-    return 1
-    # end
-    #
-    # # output the uservec with None value
-    # output_onehot = open("data_onehot.txt","w")
+    # # output the uservec ignore the None value
+    # output_onehot=open("data_onehot.txt","w")
     # for user in user_list_onehot:
-    #     #output label
+    #     # output label
     #     output_onehot.write(str(int(user[1]))+'\t')
-    #     #output index of one
+    #     # output index of one
     #     for i in range(2,len(user)):
     #         if user[i]!=0.0:
-    #             output_onehot.write(str(i)+'\t')
+    #             if i not in enc.feature_indices_:
+    #                 output_onehot.write(str(i)+'\t')
     #     output_onehot.write('\n')
     # output_onehot.close()
     # # end
+    #
+    # output the uservec with None value
+    output_onehot = open("data_onehot.txt","w")
+    for user in user_list_onehot:
+        #output label
+        output_onehot.write(str(int(user[1]))+'\t')
+        #output index of one
+        for i in range(2,len(user)):
+            if user[i]!=0.0:
+                output_onehot.write(str(i)+'\t')
+        output_onehot.write('\n')
+    output_onehot.close()
+    # end
 
+    return user_list_onehot
 #end def encode()
 
 
 # --------------------------------------------------------------------------
 # get items from s{04.05.06.07.11}.txt,and merge them into a list :item_all
-
-print 'get item list'
 item_04=get_items("./data/train/04_c_cons.tsv")
-
 item_05=get_items("./data/train/05_c_cons_prc.tsv")
 item_06=get_items("./data/train/06_cont_info.tsv")
 item_07=get_items("./data/train/07_c_rca_cons.tsv")
 item_11=get_items("./data/train/11_c_meter.tsv")
-print "item_list done!\n\n"
+
 item_all = list(set(item_04+item_05+item_06+item_07+item_11+['LABEL']))
 
 user_list_all=[]
@@ -196,79 +230,74 @@ user = {}.fromkeys(item_all)
 
 
 #get user from s{04.05.06.07.11}.txt and add them to user_list_all
-print "start get user_list in 04"
-get_user_list("./data/train/04_c_cons.tsv",item_04)
 
-print "start get user_list in 05"
+CONS_NO_list={}
+CONS_ID_list={}
+
+print '04'
+get_user_list_04("./data/train/04_c_cons.tsv",item_04)
+print '05'
 get_user_list("./data/train/05_c_cons_prc.tsv",item_05)
-
-print "start get user_list in 06"
+print '06'
 get_user_list("./data/train/06_cont_info.tsv",item_06)
-
-print "start get user_list in 07"
+print '07'
 get_user_list("./data/train/07_c_rca_cons.tsv",item_07)
-
-print "start get user_list in 11"
+print '11'
 get_user_list("./data/train/11_c_meter.tsv",item_11)
+print "end 11"
 
-print "user_list done"
 
 
 #get label_list
-print "start get label_list "
-input_label = open("train_label.txt","r")
-user_list_lable = []
+input_label = open("./data/train/train_label.csv","r")
+user_list_lable = {}
 while 1:
     lines = input_label.readlines()
     if not lines:
         break
     for line in lines:
         line=line.strip('\n')
-        user_list_lable.append(line)
+        user_list_lable[line]=1
 input_label.close()
-print "label_list done"
 
-print "start add label"
+print "add label"
 #add label to user
 for user in user_list_all:
     if user['CONS_NO'] in user_list_lable:
         user['LABEL'] = '1'
     else:
         user['LABEL'] = '0'
-print "add label done"
-		
-#all out for check
-#users before merge
-output_all = open("data_all.txt","w")
-for it in item_all:
-    output_all.write(it)
-    output_all.write('\t')
-output_all.write('\n')
-for i in user_list_all:
-    for it in item_all:
-        output_all.write(str(i[it]))
-        output_all.write('\t')
-    output_all.write('\n')
-output_all.close()
-
-#users after merge
-output_all = open("data.txt","w")
-for it in item_all:
-    output_all.write(it)
-    output_all.write('\t')
-output_all.write('\n')
-for i in user_list_all:
-    for it in item_all:
-        output_all.write(i[it])
-        output_all.write('\t')
-    output_all.write('\n')
-output_all.close()
-
+print "add label end"
+# #all out for check
+# #users before merge
+# output_all = open("data_all.txt","w")
+# for it in item_all:
+#     output_all.write(it)
+#     output_all.write('\t')
+# output_all.write('\n')
+# for i in user_list_all:
+#     for it in item_all:
+#         output_all.write(str(i[it]))
+#         output_all.write('\t')
+#     output_all.write('\n')
+# output_all.close()
+#
+# #users after merge
+# output_all = open("data.txt","w")
+# for it in item_all:
+#     output_all.write(it)
+#     output_all.write('\t')
+# output_all.write('\n')
+# for i in user_list_all:
+#     for it in item_all:
+#         output_all.write(i[it])
+#         output_all.write('\t')
+#     output_all.write('\n')
+# output_all.close()
+#
 
 #encode users
-print "start encode"
 encode(item_all,user_list_all,0,3,4,8,9,10,11,12,13,15,16,17,18,19,20,21,22,)
-print "all done"
 # LABEL-2;APPR_OPINION-5 ;ELEC_ADDR-14; CONS_ID-1,CONS_NO-7,METER_ID-6  ignored
 
 # RCA_FLAG:0	CONS_ID:1	LABEL:2	CONS_SORT_CODE:3	TYPE_CODE:4	APPR_OPINION:5
