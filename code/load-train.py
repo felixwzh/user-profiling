7213b5c63df5993ec52a250fed1c6dd475284296
felixwzh <felixwzh@outlook.com>
2016-12-09 13:05:23 +0800

    add load event file

--- code/load-train.py
+++ code/load-train.py
@@ -12,6 +12,29 @@ import re
 user=[None]*23
 cons_id_dict={}
 cons_no_dict={}
+# user_info_list=[[]]*11# something wrong if write in this way , so changed it in an ugly way
+user_info_list=[[],[],[],[],[],[],[],[],[],[],[]]
+
+# user_info_list[0]:CONS_ID
+# user_info_list[1]:23 items value in x axis
+# user_info_list[2]:23 items index
+# user_info_list[3]:23 items onehot index
+# user_info_list[4]:event_list_01
+# user_info_list[5]:event_list_02
+# user_info_list[6]:event_list_03
+# user_info_list[7]:event_list_08
+# user_info_list[8]:event_list_09
+# user_info_list[9]:event_list_10
+# user_info_list[10]:event_list_12
+
+
+
+
+
+
+
+
+
 #load from 04
 # in 04
 # CONS_ID:0	    CONS_NO:1 	        ELEC_ADDR:2	    TRADE_CODE:3  	ELEC_TYPE_CODE:4
@@ -31,6 +54,9 @@ for line in input:
         li = re.split('\t', line)
         if len(li)==12:
             user=[None]*23
+            # user_info_list=[[]]*11
+            user_info_list=[[],[],[],[],[],[],[],[],[],[],[]]
+
             cons_id=int(li[0])
             cons_no=int(li[1])
             if len(li[0])>0: user[1]=cons_id
@@ -51,8 +77,13 @@ for line in input:
             if len(li[10])>0: user[3]=li[10]
             if len(li[11])>0: user[20]=int(li[11])
 
-            cons_id_dict[cons_id]=[cons_no,user]
-            cons_no_dict[cons_no]=[cons_id,user]
+            user_info_list[0]=[cons_id]
+            # user_info_list[0].append(cons_id)
+            user_info_list[1]=user
+            cons_no_dict[cons_no]=user_info_list
+            cons_id_dict[cons_id]=user_info_list
+            # cons_id_dict[cons_id]=[cons_no,user]
+            # cons_no_dict[cons_no]=[cons_id,user]
     flag+=1
 input.close()
 print "04 end"
@@ -111,11 +142,15 @@ for line in input:
                 if len(li[3])>0: cons_no_dict[cons_no][1][9]=int(li[3])
             else:
                 user=[None]*23
+                # user_info_list=[[]]*11
+                user_info_list=[[],[],[],[],[],[],[],[],[],[],[]]
                 user[1]=cons_no
                 # if len(li[1])>0: user[5]=(li[1])
                 if len(li[2])>0: user[11]=int(li[2])
                 if len(li[3])>0: user[9]=int(li[3])
-                cons_no_dict[cons_no]=[None,user]
+                user_info_list[1]=user
+                cons_no_dict[cons_no]=user_info_list
+                # cons_no_dict[cons_no]=[None,user]
     flag+=1
 input.close()
 print "06 end"
@@ -143,12 +178,16 @@ for line in input:
                 if len(li[3])>0: cons_no_dict[cons_no][1][16]=int(li[3])
             else:
                 user=[None]*23
+                # user_info_list=[[]]*11
+                user_info_list=[[],[],[],[],[],[],[],[],[],[],[]]
                 user[1]=cons_no
                 if len(li[1])>0: user[17]=int(li[1])
                 if len(li[2])>0: user[0]=int(li[2])
                 if len(li[3])>0: user[16]=int(li[3])
                 #add this to other load part?
-                cons_no_dict[cons_no]=[None,user]
+                user_info_list[1]=user
+                cons_no_dict[cons_no]=user_info_list
+                # cons_no_dict[cons_no]=[None,user]
     flag+=1
 input.close()
 print "07 end"
@@ -230,10 +269,16 @@ def encode(*index):
 
     #add fake user
     fake_user=['other']*23
-    cons_no_dict[110]=[None,fake_user]
+    # user_info_list=[[]]*11
+    user_info_list=[[],[],[],[],[],[],[],[],[],[],[]]
+    user_info_list[1]=fake_user
+    cons_no_dict[110]=user_info_list
     #add None user
     None_user=[None]*23
-    cons_no_dict[911]=[None,None_user]
+    # user_info_list=[[]]*11
+    user_info_list=[[],[],[],[],[],[],[],[],[],[],[]]
+    user_info_list[1]=None_user
+    cons_no_dict[911]=user_info_list
 
     for i in index:
         encode_item_list_list[i]={}
@@ -245,7 +290,6 @@ def encode(*index):
 
 
     print "encode index end"
-
     # counter=1
     for user in cons_no_dict.values():
         # if counter%1000==0 :print counter
@@ -256,7 +300,7 @@ def encode(*index):
             # if user[1][i] == None :
             user_item_index[i]=encode_item_list_list[i][user[1][i]]+1#####  +1
 
-        user.append(user_item_index)#
+        user[2]=user_item_index#
         # counter+=1
     item_size_list=[None]*23
     for i in index:
@@ -268,7 +312,7 @@ def encode(*index):
         for i in index:
             user_onehot_index[i]=user[2][i]+counter
             counter+=item_size_list[i]
-        user.append(user_onehot_index) # user[3]
+        user[3]=user_onehot_index # user[3]
 
     print "new user list end"
 
@@ -313,6 +357,247 @@ def encode(*index):
 
 
 encode(0,3,4,8,9,10,11,13,15,16,17,18,19,20,21,22)
+
+#load event info
+
+# load from 01
+# user_info_list[4]:event_list_01
+
+
+# APP_NO:0	ID:1	BUSI_TYPE_CODE:2	URBAN_RURAL_FLAG:3  ORG_NO:4
+# HANDLE_TIME:5      ACCEPT_CONTENT:6	HANDLE_OPINION:7	CALLING_NO:8
+# ELEC_TYPE:9	CUST_NO:10	PROV_ORG_NO:11	CITY_ORG_NO:12
+index_01=[0,1,2,3,4,5,6,7,8,9,10,11,12]
+input=open('../data/train/01_arc_s_95598_wkst_train.tsv',"r")
+event_01=[None]*13
+flag = 0
+if len(index_01)>0:
+    for line in input:
+        if flag != 0:
+            line=line.strip('\n')
+            li = re.split('\t', line)
+            if len(li)==13:
+                if len(li[10])>0: cons_no=int(li[10])
+                event_01=[None]*13
+                for i in index_01:
+                    if len(li[i])>0: event_01[i]=li[i]
+                if cons_no in cons_no_dict:
+                    cons_no_dict[cons_no][4].append(event_01)
+                else:
+                    user_info_list=[[],[],[],[],[],[],[],[],[],[],[]]
+                    user_info_list[1]=cons_no_dict[911][1]
+                    user_info_list[2]=cons_no_dict[911][2]
+                    user_info_list[3]=cons_no_dict[911][3]
+                    user_info_list[1][7]=cons_no
+                    user_info_list[4].append(event_01)
+                    cons_no_dict[cons_no]=user_info_list
+        flag+=1
+    input.close()
+print "01 end"
+
+
+# load from 02
+# user_info_list[5]:event_list_02
+
+
+
+
+
+
+
+
+
+
+# load from 03
+# user_info_list[6]:event_list_03
+# APP_NO:0	OVERSEE_TIME:1	CUST_NO:2	CUST_NAME:3
+# OVERSEE_RESON:4	OVERSEE_CONTENT:5	OVERSEE_APP_NO:6
+# ORG_OR_DEPT:7	APP_BUSI_TYPE_CODE:8	ORG_NO:9
+index_03=[0,1,2,3,4,5,6,7,8,9]
+input=open('../data/train/03_s_info_oversee.tsv',"r")
+event_03=[None]*10
+flag = 0
+if len(index_03)>0:
+    for line in input:
+        if flag != 0:
+            line=line.strip('\n')
+            li = re.split('\t', line)
+            if len(li)==10 and len(li[2])>0:
+                cons_no=int(li[2])
+                event_03=[None]*10
+                for i in index_03:
+                    if len(li[i])>0: event_03[i]=li[i]
+                if cons_no in cons_no_dict:
+                    cons_no_dict[cons_no][6].append(event_03)
+                else:
+                    user_info_list=[[],[],[],[],[],[],[],[],[],[],[]]
+                    user_info_list[1]=cons_no_dict[911][1]
+                    user_info_list[2]=cons_no_dict[911][2]
+                    user_info_list[3]=cons_no_dict[911][3]
+                    user_info_list[1][7]=cons_no
+                    user_info_list[6].append(event_03)
+                    cons_no_dict[cons_no]=user_info_list
+        flag+=1
+    input.close()
+
+print "03 end"
+
+
+
+
+# load from 08
+# user_info_list[7]:event_list_08
+# 77777777777777777777777777777777777777777
+# ORG_NO:0	CONS_NO:1	RCVED_YM:2	RCVED_DATE:3
+# THIS_RCVED_AMT:4	THIS_PENALTY:5	OWE_AMT:6	RCVBL_YM:7	RCVBL_PENALTY:8
+index_08=[0,1,2,3,4,5,6,7,8]
+input=open('../data/train/08_a_rcved_flow.tsv',"r")
+event_08=[None]*9
+flag = 0
+if len(index_08)>0:
+    for line in input:
+        if flag != 0:
+            line=line.strip('\n')
+            li = re.split('\t', line)
+            if len(li)==9 and len(li[1])>0:
+                cons_no=int(li[1])
+                event_08=[None]*9
+                for i in index_08:
+                    if len(li[i])>0: event_08[i]=li[i]
+                if cons_no in cons_no_dict:
+                    cons_no_dict[cons_no][7].append(event_08)
+                else:
+                    user_info_list=[[],[],[],[],[],[],[],[],[],[],[]]
+                    user_info_list[1]=cons_no_dict[911][1]
+                    user_info_list[2]=cons_no_dict[911][2]
+                    user_info_list[3]=cons_no_dict[911][3]
+                    user_info_list[1][7]=cons_no
+                    user_info_list[7].append(event_08)
+                    cons_no_dict[cons_no]=user_info_list
+        flag=1
+    input.close()
+
+print "08 end"
+
+
+
+
+
+
+
+# load from 09
+# user_info_list[8]:event_list_09
+# 88888888888888888888888888888888888888888888888888888888
+# CONS_NO:0	RCVBL_YM:1	ORG_NO:2	PAY_MODE:3
+# T_PQ:4	RCVBL_AMT:5	RCVED_AMT:6	STATUS_CODE:7
+# RCVBL_PENALTY:8	RCVED_PENALTY:9	RISK_LEVEL_CODE:10	OWE_AMT:11
+# CONS_SORT_CODE:12	ELEC_TYPE_CODE:13	CTL_MODE:14
+index_09=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14]
+input=open('../data/train/09_arc_a_rcvbl_flow.tsv',"r")
+event_09=[None]*15
+flag = 0
+if len(index_09)>0:
+    for line in input:
+        if flag != 0:
+            line=line.strip('\n')
+            li = re.split('\t', line)
+            if len(li)==15 and len(li[0])>0:
+                cons_no=int(li[0])
+                event_09=[None]*15
+                for i in index_09:
+                    if len(li[i])>0: event_09[i]=li[i]
+                if cons_no in cons_no_dict:
+                    cons_no_dict[cons_no][8].append(event_09)
+                else:
+                    user_info_list=[[],[],[],[],[],[],[],[],[],[],[]]
+                    user_info_list[1]=cons_no_dict[911][1]
+                    user_info_list[2]=cons_no_dict[911][2]
+                    user_info_list[3]=cons_no_dict[911][3]
+                    user_info_list[1][7]=cons_no
+                    user_info_list[8].append(event_09)
+                    cons_no_dict[cons_no]=user_info_list
+        flag=1
+    input.close()
+
+print "09 end"
+
+
+
+
+
+
+
+# load from 10
+# user_info_list[9]:event_list_10
+
+
+
+
+
+
+
+
+
+# load from 12
+# user_info_list[10]:event_list_12
+# CONS_NO:0	ORG_NO:1	CHARGE_YM:2	CHARGE_DATE:3	PAY_MODE:4
+index_12=[0,1,2,3,4]
+input=open('../data/train/12_a_pay_flow.tsv',"r")
+event_12=[None]*5
+flag = 0
+if len(index_12)>0:
+    for line in input:
+        if flag != 0:
+            line=line.strip('\n')
+            li = re.split('\t', line)
+            if len(li)==5 and len(li[0])>0:
+                cons_no=int(li[0])
+                event_12=[None]*5
+                for i in index_12:
+                    if len(li[i])>0: event_12[i]=li[i]
+                if cons_no in cons_no_dict:
+                    cons_no_dict[cons_no][10].append(event_12)
+                else:
+                    user_info_list=[[],[],[],[],[],[],[],[],[],[],[]]
+                    user_info_list[1]=cons_no_dict[911][1]
+                    user_info_list[2]=cons_no_dict[911][2]
+                    user_info_list[3]=cons_no_dict[911][3]
+                    user_info_list[1][7]=cons_no
+                    user_info_list[10].append(event_12)
+                    cons_no_dict[cons_no]=user_info_list
+        flag=1
+    input.close()
+
+print "12 end"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
